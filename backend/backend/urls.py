ccf960ae550e15552e154afa09c7a4b38bc7f78a
SushiRocket <kentamori27@gmail.com>
2025-01-27 00:54:40 +0000

    docs: Add directry composition of file on top.

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
+# backend/backend/urls.py
+
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
